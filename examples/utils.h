41e3bcaf64d6ac74135bf9baa48bd739049c37e7
Zimin Li <coollizimin@gmail.com>
2026-04-15 12:38:56 +0000

    feat: create `examples/utils.h` and support `infiniFinalize()`  - support `infiniFinalize()` and add its ompi's implementation, used in `examples/all_reduce.cc`  - create `examples/utils.h` for having all the utilities used by the example programs and move the `CHECK_INFINI` macro into it

---
+++ examples/utils.h
@@ -0,0 +1,14 @@
+#ifndef INFINI_CCL_EXAMPLES_UTILS_H_
+#define INFINI_CCL_EXAMPLES_UTILS_H_
+
+// Simple check macro for the C-API
+#define CHECK_INFINI(cmd)                                                      \
+  do {                                                                         \
+    infiniResult_t res = (cmd);                                                \
+    if (res != infiniSuccess) {                                                \
+      std::cerr << "InfiniCCL Error at " << __LINE__ << std::endl;             \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
+
+#endif
