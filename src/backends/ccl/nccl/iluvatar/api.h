54423a600ad1ff2673535d2e2ae0c39762f6939e
Zimin Li <coollizimin@gmail.com>
2026-07-14 09:28:14 +0000

    style: clang-format unformatted file

--- src/backends/ccl/nccl/iluvatar/api.h
+++ src/backends/ccl/nccl/iluvatar/api.h
@@ -1,8 +1,8 @@
 #ifndef INFINI_CCL_BACKENDS_CCL_NCCL_ILUVATAR_API_H_
 #define INFINI_CCL_BACKENDS_CCL_NCCL_ILUVATAR_API_H_
 
-#include "devices/iluvatar/runtime_.h"
 #include "backends/ccl/nccl/api.h"
+#include "devices/iluvatar/runtime_.h"
 
 namespace infini::ccl {
 
